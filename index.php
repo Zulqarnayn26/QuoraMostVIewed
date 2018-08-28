b4b727222ae05eaa0dac45df319a93098b5d08d7
Zulqarnayn26 <34345953+Zulqarnayn26@users.noreply.github.com>
2018-08-28 07:27:49 +0600

    v 1.1(with hosting)

--- index.php
+++ index.php
@@ -1,15 +1,73 @@
+<html>
+<head>
+<style> 
+input[type=text] {
+    width: 100%;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 15px;
+    background-color: white;
+    background-position: 10px 10px; 
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 10px;
+}
+
+
+.btn-group .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 9px 18px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+    float: left;
+}
+
+.btn-group .button:hover {
+    background-color: #3e8e41;
+}
+</style>
+</head>
+<body>
+
+<form>
+  
+</form>
+
+<form action="" method="post">
+	<input type="text" name="name" placeholder="Put any Quora's question link. Ex: https://www.quora.com/How-does-it-feel-to-be-extremely-poor">
+	<div class="btn-group">
+		<button class="button">Submit</button>
+	</div>
+</form>
+
+</body>
+</html>
+
 <?php
 	include("getData.php");
 	function space() {
     	echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
 	}
 
-	$content = getData("https://www.quora.com/Is-the-never-give-up-attitude-always-helpful");
-	$exploded_content = explode("<img class='profile_photo_img' src='", $content);
+	//print_r($_REQUEST);
+	if(isset($_POST["name"])){
+		$val = $_POST["name"];
+		if(empty($val)) $val = "https://www.quora.com/How-does-it-feel-to-be-extremely-poor";
+		$content = getData($val);
+	}
+	else {
+		$content = getData("https://www.quora.com/How-does-it-feel-to-be-extremely-poor");	
+	}
 	
+	$exploded_content = explode("<img class='profile_photo_img' src='", $content);
 	$content_name1 = explode("</script><title>", $content);
 	$content_name2 = explode(' - Quora</title><meta', $content_name1[1]);
-	echo "<h1 style='color:#3498DB;'>".$content_name2[0] ."</h1>";
+	echo "<h1 style='color:#3498DB;'>\n\n\n\n\n\n\n\n\n".$content_name2[0] ."</h1>";
 
 	for ($x = 1; $x < sizeof($exploded_content); $x++) {
 		//image
@@ -44,7 +102,12 @@
 			$userId2 = explode("' action_mousedown='UserLinkClickthrough' id='", $userId1[1]);
 			echo "<b>Profile : </b><a class='user' href='https://www.quora.com/profile/".$userId2[0]."'style='text-decoration:none'>$name2[1]</a>";
 		}
+
+		//upvote
 		
 		echo "<br> <br>";
+
 	}
+	echo "<font size='5'>Find me in <a href='https://www.wordpresscom77092.wordpress.com' style='text-decoration:none'>  &nbsp Wordpress </a> <a href='https://github.com/Zulqarnayn26' style='text-decoration:none'> &nbsp&nbsp Github </a> <a href='https://www.linkedin.com/in/asif-mujtaba-084876147/' style='text-decoration:none'> &nbsp&nbsp linkedin</a></font>"; 
+	
 ?>
